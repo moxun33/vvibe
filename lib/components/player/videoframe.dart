9ce9543d9d33fae3fd60d5acd2b6c47156e9ec22
Moxx <moxingxun@zhiyunshuke.com>
2022-09-29 17:11:48 +0800

    fix:bugs

--- lib/components/player/videoframe.dart
+++ lib/components/player/videoframe.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:dart_vlc/dart_vlc.dart';
 import 'package:vvibe/models/playlist_item.dart';
