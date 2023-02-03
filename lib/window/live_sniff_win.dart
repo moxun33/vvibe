a01ad5686b7d132eb51816ac8e56a6a937d97129
moxun33 <mxx0807@gmail.com>
2023-02-03 23:05:07 +0800

    fix sub window

--- lib/window/live_sniff_win.dart
+++ lib/window/live_sniff_win.dart
@@ -6,6 +6,7 @@ import 'package:vvibe/common/langs/translation_service.dart';
 import 'package:vvibe/components/sniff/live_sniff.dart';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:vvibe/services/notifications/v_size_changed_layout_notification.dart';
+import 'package:vvibe/window/sub_window.dart';
 
 class LiveSniffWin extends StatefulWidget {
   const LiveSniffWin(
@@ -30,27 +31,12 @@ class _LiveSniffWinState extends State<LiveSniffWin> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: NotificationListener<VSizeChangedLayoutNotification>(
-          onNotification: (VSizeChangedLayoutNotification notification) {
-            if (notification.size.width < 1200 ||
-                notification.size.height < 700) {
-              widget.windowController
-                ..setFrame(const Offset(0, 0) & const Size(1280, 720))
-                ..center();
-            }
-            return true;
-          },
-          child: VSizeChangedLayoutNotifier(
-              child: Container(
-            child: LiveSniff(),
-            constraints: BoxConstraints(minWidth: 1200, minHeight: 700),
-          ))),
+    return SubWindow(
+      windowController: widget.windowController,
+      args: widget.args,
+      child: LiveSniff(),
       title: 'VVibe 直播源扫描',
       theme: widget.theme,
-      debugShowCheckedModeBanner: false,
-      builder: EasyLoading.init(),
-      locale: TranslationService.locale,
     );
   }
 }
---
+++ lib/window/sub_window.dart
@@ -0,0 +1,59 @@
+//子窗口
+
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:vvibe/common/langs/translation_service.dart';
+import 'package:desktop_multi_window/desktop_multi_window.dart';
+import 'package:vvibe/services/notifications/v_size_changed_layout_notification.dart';
+
+class SubWindow extends StatefulWidget {
+  const SubWindow(
+      {Key? key,
+      required this.windowController,
+      required this.args,
+      required this.title,
+      required this.child,
+      required this.theme})
+      : super(key: key);
+  final ThemeData theme;
+  final WindowController windowController;
+  final Map? args;
+  final String title;
+  final Widget child;
+
+  @override
+  _SubWindowState createState() => _SubWindowState();
+}
+
+class _SubWindowState extends State<SubWindow> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: NotificationListener<VSizeChangedLayoutNotification>(
+          onNotification: (VSizeChangedLayoutNotification notification) {
+            if (notification.size.width < 1200 ||
+                notification.size.height < 700) {
+              widget.windowController
+                ..setFrame(const Offset(0, 0) & const Size(1280, 720))
+                ..center();
+            }
+            return true;
+          },
+          child: VSizeChangedLayoutNotifier(
+              child: Container(
+            child: widget.child,
+            constraints: BoxConstraints(minWidth: 1200, minHeight: 700),
+          ))),
+      title: widget.title,
+      theme: widget.theme,
+      debugShowCheckedModeBanner: false,
+      builder: EasyLoading.init(),
+      locale: TranslationService.locale,
+    );
+  }
+}
